3cd021b9bcc61781301e4570b3325d30aaf3f549
Jakub Jodelka <jakub@jodelka.me>
2016-11-03 15:27:06 +0100

    [sample] fix after merge and add glide to sample build.gradle

--- sample/src/main/java/com/mateuszkoslacz/moviper/sample/viper/interactor/RxInteractor.java
+++
@@ -1,8 +0,0 @@
-package com.mateuszkoslacz.moviper.sample.viper.interactor;
-
-import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor;
-import com.mateuszkoslacz.moviper.sample.viper.contract.RxContract;
-
-public class RxInteractor extends BaseRxInteractor implements RxContract.Interactor {
-    
-}
