72282bf7f43a84457bba8a56769cf2e9bb0f68be
mkoslacz <mateusz.koslacz@grupawp.pl>
2016-11-21 10:55:49 +0100

    [rx-tests] find the solution to the problematic test-case

--- rxsample/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/ListingPresenterTest.java
+++ rxsample/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/ListingPresenterTest.java
@@ -1,7 +1,5 @@
 package com.mateuszkoslacz.moviper.rxsample.viper.presenter;
 
-import android.app.Activity;
-
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
 import com.mateuszkoslacz.moviper.rxsample.viper.interactor.ListingInteractor;
 import com.mateuszkoslacz.moviper.rxsample.viper.presenter.utils.RxAndroidSchedulersOverrideRule;
@@ -15,24 +13,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Observable;
-import rx.Scheduler;
-import rx.android.plugins.RxAndroidPlugins;
-import rx.android.plugins.RxAndroidSchedulersHook;
-import rx.observers.TestSubscriber;
-import rx.plugins.RxJavaPlugins;
-import rx.plugins.RxJavaSchedulersHook;
-import rx.schedulers.Schedulers;
 import rx.schedulers.TestScheduler;
 import rx.subjects.TestSubject;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by mateuszkoslacz on 17.11.2016.
@@ -40,9 +27,8 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class ListingPresenterTest {
 
-    // it doesn't seem to work
-//    @Rule
-//    public final RxAndroidSchedulersOverrideRule mOverrideRule = new RxAndroidSchedulersOverrideRule();
+    @Rule
+    public final RxAndroidSchedulersOverrideRule mOverrideRule = new RxAndroidSchedulersOverrideRule();
 
     @Mock
     private ListingInteractor mInteractor;
@@ -57,7 +43,7 @@ public class ListingPresenterTest {
     private ListingPresenter mPresenter = new ListingPresenter(mView);
 
     @Before
-    public void setUpPresenter(){
+    public void setUpPresenter() {
         mPresenter.attachView(mView);
     }
 
@@ -65,11 +51,13 @@ public class ListingPresenterTest {
     @Test
     public void onViewCreated() throws Exception {
         List<User> users = new ArrayList<>();
-        TestSubject<List<User>> subject = TestSubject.create(new TestScheduler());
+        TestScheduler scheduler = new TestScheduler();
+        TestSubject<List<User>> subject = TestSubject.create(scheduler);
         Mockito.when(mInteractor.getUserList()).thenReturn(subject);
         mPresenter.onViewCreated();
         Mockito.verify(mView).showLoading();
         subject.onNext(users);
+        scheduler.triggerActions();
         Mockito.verify(mInteractor).getUserList();
         Mockito.verify(mView).setUserList(users);
         Mockito.verify(mView).showContent();
@@ -77,27 +65,29 @@ public class ListingPresenterTest {
 
     @Test
     public void onItemClicked() throws Exception {
+        // read more:
+        // http://code.hootsuite.com/unit-testing-with-rxandroid-and-rxjava/
+
         List<User> users = new ArrayList<>();
         TestScheduler scheduler = new TestScheduler();
         TestSubject<List<User>> subject = TestSubject.create(scheduler);
         Mockito.when(mInteractor.getUserList()).thenReturn(subject);
-        // adjust tests to make printlns in subscribe appear
-        // http://code.hootsuite.com/unit-testing-with-rxandroid-and-rxjava/
         mInteractor.getUserList()
-                .subscribeOn(scheduler)
-                .observeOn(scheduler)
+//                .subscribeOn(scheduler)
+//                .observeOn(scheduler)
                 .subscribe(
-                users1 -> {
-                    System.out.println("onNext: " + users1);
-                },
-                throwable -> {
-                    System.out.println("onError: " + throwable.getLocalizedMessage());
-                    throwable.printStackTrace();
-                },
-                () -> {
-                    System.out.println("onCompleted");
-                }
-        );
+                        users1 -> {
+                            System.out.println("onNext: " + users1);
+                        },
+                        throwable -> {
+                            System.out.println("onError: " + throwable.getLocalizedMessage());
+                            throwable.printStackTrace();
+                        },
+                        () -> {
+                            System.out.println("onCompleted");
+                        }
+                );
         subject.onNext(users);
+        scheduler.triggerActions();
     }
 }
--- rxsample/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/utils/RxAndroidSchedulersOverrideRule.java
+++ rxsample/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/utils/RxAndroidSchedulersOverrideRule.java
@@ -12,10 +12,10 @@ import rx.plugins.RxJavaSchedulersHook;
 import rx.schedulers.Schedulers;
 
 /**
- * This rule registers SchedulerHooks for RxJava and RxAndroid to ensure that subscriptions
- * always subscribeOn and observeOn Schedulers.immediate().
- * Warning, this rule will reset RxAndroidPlugins and RxJavaPlugins before and after each test so
- * if the application code uses RxJava plugins this may affect the behaviour of the testing method.
+ * This rule registers SchedulerHooks for RxJava and RxAndroid to ensure that subscriptions always
+ * subscribeOn and observeOn Schedulers.immediate(). Warning, this rule will reset RxAndroidPlugins
+ * and RxJavaPlugins before and after each test so if the application code uses RxJava plugins this
+ * may affect the behaviour of the testing method.
  */
 public class RxAndroidSchedulersOverrideRule implements TestRule {
 
@@ -27,6 +27,11 @@ public class RxAndroidSchedulersOverrideRule implements TestRule {
 
         @Override
         public Scheduler getNewThreadScheduler() {
+            return Schedulers.immediate();
+        }
+
+        @Override
+        public Scheduler getComputationScheduler() {
             return Schedulers.immediate();
         }
     };
@@ -43,17 +48,17 @@ public class RxAndroidSchedulersOverrideRule implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                RxAndroidPlugins.getInstance().reset();
-                RxAndroidPlugins.getInstance().registerSchedulersHook(mRxAndroidSchedulersHook);
-
-                RxJavaPlugins.getInstance().reset();
-                RxJavaPlugins.getInstance().registerSchedulersHook(mRxJavaSchedulersHook);
-
+                overrideHooks();
                 base.evaluate();
-
-                RxAndroidPlugins.getInstance().reset();
-                RxJavaPlugins.getInstance().reset();
             }
         };
+    }
+
+    private void overrideHooks() {
+        RxAndroidPlugins.getInstance().reset();
+        RxAndroidPlugins.getInstance().registerSchedulersHook(mRxAndroidSchedulersHook);
+
+        RxJavaPlugins.getInstance().reset();
+        RxJavaPlugins.getInstance().registerSchedulersHook(mRxJavaSchedulersHook);
     }
 }
