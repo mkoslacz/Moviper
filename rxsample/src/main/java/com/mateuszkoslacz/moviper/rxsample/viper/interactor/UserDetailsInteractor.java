4e66f5c0d4d40ea0ea582082fe98582a98ee40d3
mkoslacz <mateusz.koslacz@grupawp.pl>
2016-11-24 14:23:03 +0100

    [rx-sample][rdp][interactors] improv: use rdp in interactors

--- rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/interactor/UserDetailsInteractor.java
+++ rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/interactor/UserDetailsInteractor.java
@@ -1,32 +1,27 @@
 package com.mateuszkoslacz.moviper.rxsample.viper.interactor;
 
 import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor;
-import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
-import com.mateuszkoslacz.moviper.rxsample.data.retrofit.GitHubApiInterface;
+import com.mateuszkoslacz.moviper.rxsample.data.rdp.repository.Repository;
+import com.mateuszkoslacz.moviper.rxsample.data.rdp.repository.retrofit.UserRetrofitRepository;
+import com.mateuszkoslacz.moviper.rxsample.data.rdp.specification.stream.UserByUsernameStreamSpecification;
+import com.mateuszkoslacz.moviper.rxsample.data.rdp.specification.stream.retrofit.impl.UserByUsernameRetrofitStreamSpecification;
 import com.mateuszkoslacz.moviper.rxsample.viper.contract.UserDetailsContract;
+import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
 
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
 
 public class UserDetailsInteractor
         extends BaseRxInteractor
         implements UserDetailsContract.Interactor {
 
-    private GitHubApiInterface mGitHubApiInterface;
-
-    public UserDetailsInteractor() {
-        final Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(GitHubApiInterface.GITHUB_API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-        mGitHubApiInterface = retrofit.create(GitHubApiInterface.class);
-    }
+    private Repository<User> mUserRepository = new UserRetrofitRepository();
 
     @Override
     public Observable<User> getUserForUsername(String user) {
-        return mGitHubApiInterface.getUserForUsername(user);
+        return mUserRepository.streamQuery(getUserByUsernameSpecification(user));
+    }
+
+    private UserByUsernameStreamSpecification getUserByUsernameSpecification(String username) {
+        return new UserByUsernameRetrofitStreamSpecification().setUserName(username);
     }
 }
