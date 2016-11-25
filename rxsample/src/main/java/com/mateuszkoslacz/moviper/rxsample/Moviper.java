dcb1e8cbe3dd6f48df7b84c2a400ad2b9fa1953d
bwilk <bartosz.wilk@grupawp.pl>
2016-11-25 12:21:49 +0100

    [rx-sample][rdp][dagger] add: custom Moviper application class

---
+++ rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/Moviper.java
@@ -0,0 +1,12 @@
+package com.mateuszkoslacz.moviper.rxsample;
+
+import android.app.Application;
+
+
+public class Moviper extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
