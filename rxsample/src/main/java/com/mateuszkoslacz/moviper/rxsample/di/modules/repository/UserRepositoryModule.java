c11da5d131b89d139d287495b70fd44ccf2b659f
bwilk <bartosz.wilk@grupawp.pl>
2016-11-29 17:48:35 +0100

    [rx-sample][rdp][dagger] provide UserRetrofitRepository as a singleton

--- rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/di/modules/repository/UserRepositoryModule.java
+++ rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/di/modules/repository/UserRepositoryModule.java
@@ -4,6 +4,8 @@ import com.mateuszkoslacz.moviper.rxsample.data.rdp.repository.Repository;
 import com.mateuszkoslacz.moviper.rxsample.data.rdp.repository.retrofit.UserRetrofitRepository;
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -11,6 +13,7 @@ import dagger.Provides;
 public class UserRepositoryModule {
 
     @Provides
+    @Singleton
     public Repository<User> provideUserRepository() {
         return new UserRetrofitRepository();
     }
