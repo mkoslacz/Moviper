759a2af7627fbb3fd609640462f57a59a80b34a9
bwilk <bartosz.wilk@grupawp.pl>
2016-11-29 17:51:43 +0100

    [rx-sample][rdp][dagger] cleanup: MoviperTestRunner class

--- rxsample/src/androidTest/java/com/mateuszkoslacz/moviper/rxsample/utils/MoviperTestRunner.java
+++ rxsample/src/androidTest/java/com/mateuszkoslacz/moviper/rxsample/utils/MoviperTestRunner.java
@@ -7,7 +7,8 @@ import android.support.test.runner.AndroidJUnitRunner;
 public class MoviperTestRunner extends AndroidJUnitRunner {
 
     @Override
-    public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+    public Application newApplication(ClassLoader cl, String className, Context context)
+            throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return super.newApplication(cl, MoviperTestApplication.class.getName(), context);
     }
 }
