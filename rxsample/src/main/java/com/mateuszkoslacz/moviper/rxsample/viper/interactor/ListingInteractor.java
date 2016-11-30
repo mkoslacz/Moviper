bcee0a5278588c0b7cce048f2eb82a5aa7d5a438
mkoslacz <mateusz.koslacz@grupawp.pl>
2016-11-30 10:42:30 +0100

    Revert "Merge pull request #10 from bartoszwilk/rxsample-rdp-dagger2-tests"
    This reverts commit 1d93d8bf1920071c3a7f36e1b584e9e8f760f809.

--- rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/interactor/ListingInteractor.java
+++ rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/interactor/ListingInteractor.java
@@ -1,29 +1,34 @@
 package com.mateuszkoslacz.moviper.rxsample.viper.interactor;
 
 import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor;
-import com.mateuszkoslacz.moviper.rxsample.data.rdp.repository.Repository;
-import com.mateuszkoslacz.moviper.rxsample.di.DIProvider;
-import com.mateuszkoslacz.moviper.rxsample.viper.contract.ListingContract;
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
+import com.mateuszkoslacz.moviper.rxsample.data.retrofit.GitHubApiInterface;
+import com.mateuszkoslacz.moviper.rxsample.viper.contract.ListingContract;
 
 import java.util.List;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
 
 public class ListingInteractor
         extends BaseRxInteractor
         implements ListingContract.Interactor {
 
-    private Repository<User> mUserRepository;
+    private GitHubApiInterface mGitHubApiInterface;
 
     public ListingInteractor() {
-        mUserRepository = DIProvider.getRepositoryComponent().provideUserRepository();
+        Retrofit mRetrofit = new Retrofit.Builder()
+                .baseUrl(GitHubApiInterface.GITHUB_API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+        mGitHubApiInterface = mRetrofit.create(GitHubApiInterface.class);
     }
 
     @Override
     public Observable<List<User>> getUserList() {
-        return mUserRepository.query(DIProvider
-                .getSpecificationComponent()
-                .provideAllUsersSpecification());
+        return mGitHubApiInterface.getUsers();
     }
 }
