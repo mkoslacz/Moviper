c376bac1e7829cea4795f5e6b0795a19e622f6d9
Jakub Jodelka <jakub@jodelka.me>
2016-12-01 15:34:48 +0100

    [core][view-holder] add: ViewHolder base classes without ViewHelper

---
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/routing/ViewHolderBaseRxRouting.java
@@ -0,0 +1,33 @@
+package com.mateuszkoslacz.moviper.base.routing;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.mateuszkoslacz.moviper.iface.routing.MoviperRxRouting;
+import com.mateuszkoslacz.moviper.util.WeakReferenceUtils;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by jjodelka on 01/12/2016.
+ */
+
+public class ViewHolderBaseRxRouting
+        extends ActivityBaseRxRouting
+        implements MoviperRxRouting {
+
+    @Nullable
+    private WeakReference<Activity> activity;
+
+    public ViewHolderBaseRxRouting(@NonNull Activity activity) {
+        super(activity);
+        this.activity = new WeakReference<>(activity);
+    }
+
+    @Override
+    public void onPresenterDetached() {
+        super.onPresenterDetached();
+        WeakReferenceUtils.detach(activity);
+    }
+}
