0e9e972c2c6de447c43224eca723faa204e729ea
Tomasz Najda <tomasz.najda@grupawp.pl>
2016-12-02 12:55:22 +0100

    [rxsample][ListingActivity][fix] reference to the adapter should be cleared in onDestroy instead of onStop

--- rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/view/activity/ListingActivity.java
+++ rxsample/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/view/activity/ListingActivity.java
@@ -86,8 +86,8 @@ public class ListingActivity
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         mAdapter = null;
     }
 
