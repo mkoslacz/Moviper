71689df53e283bbddfea1e0b281602b6d13f2246
bwilk <bartosz.wilk@grupawp.pl>
2016-12-08 10:18:31 +0100

    [ipcsample] refactor ColorWidgetFragment instrumentation test

--- ipcsample/src/androidTest/java/com/mateuszkoslacz/moviper/ipcsample/ColorWidgetFragmentInstrumentationTest.java
+++ ipcsample/src/androidTest/java/com/mateuszkoslacz/moviper/ipcsample/ColorWidgetFragmentInstrumentationTest.java
@@ -1,9 +1,11 @@
 package com.mateuszkoslacz.moviper.ipcsample;
 
+import android.os.Bundle;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.mateuszkoslacz.moviper.ipcsample.viper.view.fragment.ColorWidgetFragment;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +15,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.doesNotExis
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by bwilk on 12/5/16.
@@ -24,14 +27,28 @@ public class ColorWidgetFragmentInstrumentationTest {
     public FragmentTestRule<ColorWidgetFragment>
             mFragmentTestRule = new FragmentTestRule<>(ColorWidgetFragment.class);
 
+    /*
+        Here we can perform isolated fragment tests.
+        However, in this sample, our fragment doesn't change it's state while performing
+        any actions on his views.
+     */
+
     @Test
-    public void testFragment() {
+    public void shouldFragmentBeLoadedSuccessfully() {
         mFragmentTestRule.launchActivity(null);
         onView(withId(R.id.button_first)).check(matches(isDisplayed()));
-        mFragmentTestRule.getFragmentManager()
-                .beginTransaction()
-                .remove(mFragmentTestRule.getFragment())
-                .commit();
+    }
+
+    @Test
+    public void shouldFragmentBeDestroyedSuccessfully() {
+        mFragmentTestRule.launchActivity(null);
+        onView(withId(R.id.button_first)).check(matches(isDisplayed()));
+        mFragmentTestRule.removeFragment();
         onView(withId(R.id.button_first)).check(doesNotExist());
+    }
+
+    @After
+    public void cleanUp() {
+        mFragmentTestRule.removeFragment();
     }
 }
