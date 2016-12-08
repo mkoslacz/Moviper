4a50a57a447adb19fd96e34c02337f6a1807d755
bwilk <bartosz.wilk@grupawp.pl>
2016-12-08 09:56:12 +0100

    [ipcsample][FragmentTestRule] add: setting args possibility

--- ipcsample/src/androidTest/java/com/mateuszkoslacz/moviper/ipcsample/FragmentTestRule.java
+++ ipcsample/src/androidTest/java/com/mateuszkoslacz/moviper/ipcsample/FragmentTestRule.java
@@ -17,6 +17,7 @@ public class FragmentTestRule<T extends MvpFragment> extends ActivityTestRule<Te
     private final Class<T> mFragmentClass;
     private T mFragment;
     private FragmentManager mFragmentManager;
+    private Bundle mArguments;
 
     public FragmentTestRule(final Class<T> fragmentClass) {
         super(TestActivity.class, true, false);
@@ -36,18 +37,22 @@ public class FragmentTestRule<T extends MvpFragment> extends ActivityTestRule<Te
                 mFragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction transaction = mFragmentManager.beginTransaction();
                 mFragment = mFragmentClass.newInstance();
-                mFragment.setArguments(new Bundle());
+                mFragment.setArguments(mArguments != null ? mArguments : new Bundle());
                 transaction.replace(R.id.container, mFragment);
                 transaction.commit();
             } catch (InstantiationException | IllegalAccessException e) {
                 Assert.fail(
                         String.format("%s: Could not insert %s into TestActivity: %s",
-                        getClass().getSimpleName(),
-                        mFragmentClass.getSimpleName(),
-                        e.getMessage())
+                                getClass().getSimpleName(),
+                                mFragmentClass.getSimpleName(),
+                                e.getMessage())
                 );
             }
         });
+    }
+
+    public void setArguments(Bundle arguments) {
+        mArguments = arguments;
     }
 
     public T getFragment() {
