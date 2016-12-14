dcc7762dec16aaaac04af9af697c3a05d7e4e5e3
Tomasz Najda <tomasz.najda@grupawp.pl>
2016-12-14 14:51:06 +0100

    [sample][rx] improv: adapt sample to the new routing lifecycle

--- sample-rx/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRouting.java
+++ sample-rx/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRouting.java
@@ -1,8 +1,5 @@
 package com.mateuszkoslacz.moviper.rxsample.viper.routing;
 
-import android.app.Activity;
-import android.support.annotation.NonNull;
-
 import com.mateuszkoslacz.moviper.base.routing.BaseRxRouting;
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
 import com.mateuszkoslacz.moviper.rxsample.viper.contract.ListingContract;
@@ -11,10 +8,6 @@ import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.UserDetailsActivi
 public class ListingRouting
         extends BaseRxRouting
         implements ListingContract.Routing {
-
-    public ListingRouting(@NonNull Activity activity) {
-        super(activity);
-    }
 
     @Override
     public void startUserDetailsActivity(User user) {
