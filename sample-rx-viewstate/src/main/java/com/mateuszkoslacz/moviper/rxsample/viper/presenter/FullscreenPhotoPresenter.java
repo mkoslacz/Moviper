4170eb04170205573856ce58e8e7c7944daae530
Tomasz Najda <tomasz.najda@grupawp.pl>
2016-12-14 16:23:43 +0100

    [sample][rx-viewstate] improv: adapt sample to new presenters

--- sample-rx-viewstate/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/FullscreenPhotoPresenter.java
+++ sample-rx-viewstate/src/main/java/com/mateuszkoslacz/moviper/rxsample/viper/presenter/FullscreenPhotoPresenter.java
@@ -3,17 +3,16 @@ package com.mateuszkoslacz.moviper.rxsample.viper.presenter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
-import com.mateuszkoslacz.moviper.base.presenter.ViperActivityBaseRxPresenter;
+import com.mateuszkoslacz.moviper.base.presenter.ViperBaseRxPresenter;
 import com.mateuszkoslacz.moviper.rxsample.viper.contract.FullscreenPhotoContract;
 import com.mateuszkoslacz.moviper.rxsample.viper.interactor.FullscreenPhotoInteractor;
 import com.mateuszkoslacz.moviper.rxsample.viper.routing.FullscreenPhotoRouting;
 import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.FullscreenPhotoActivity;
 
 public class FullscreenPhotoPresenter
-        extends ViperActivityBaseRxPresenter
-        <FullscreenPhotoContract.View,
-                FullscreenPhotoContract.Interactor,
-                FullscreenPhotoContract.Routing>
+        extends ViperBaseRxPresenter<FullscreenPhotoContract.View,
+                        FullscreenPhotoContract.Interactor,
+                        FullscreenPhotoContract.Routing>
         implements FullscreenPhotoContract.Presenter {
 
     private String mPhotoUrl;
