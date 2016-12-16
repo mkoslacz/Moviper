b3763b0882f18bbc4eb0972415a197e59a933825
Tomasz Najda <tomasz.najda@grupawp.pl>
2016-12-16 15:06:28 +0100

    [sample][rx-presenter] improv: adapt sample to recent changes

--- sample-rx-presenter/src/main/java/com/mateuszkoslacz/rxpresenter/viper/contract/SampleContract.java
+++ sample-rx-presenter/src/main/java/com/mateuszkoslacz/rxpresenter/viper/contract/SampleContract.java
@@ -3,9 +3,9 @@ package com.mateuszkoslacz.rxpresenter.viper.contract;
 import android.os.Bundle;
 
 import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby.mvp.MvpView;
 import com.mateuszkoslacz.moviper.iface.interactor.ViperRxInteractor;
 import com.mateuszkoslacz.moviper.iface.routing.ViperRxRouting;
-import com.mateuszkoslacz.moviper.iface.view.ViperView;
 
 public interface SampleContract {
 
@@ -14,7 +14,7 @@ public interface SampleContract {
         void onViewCreated(Bundle savedInstanceState);
     }
 
-    interface View extends ViperView {
+    interface View extends MvpView {
 
         void showNumber(long number);
     }
