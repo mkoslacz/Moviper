e107df499ac74bdf29d4f6e77b12ca34ba92aea9
mkoslacz <mateusz.koslacz@grupawp.pl>
2016-12-22 15:11:40 +0100

    [exceptions][docs] add: documentation

--- moviper/src/main/java/com/mateuszkoslacz/moviper/base/exception/PresentersAccessUtilNotEnabled.java
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/exception/PresentersAccessUtilNotEnabled.java
@@ -1,7 +1,17 @@
 package com.mateuszkoslacz.moviper.base.exception;
 
+import android.app.Application;
+
+import com.mateuszkoslacz.moviper.presenterbus.Config;
+import com.mateuszkoslacz.moviper.presenterbus.Moviper;
+
 /**
  * Created by mateuszkoslacz on 24.10.2016.
+ * <p/>
+ * Thrown when you haven't enabled IPC and you try to use it ({@link Moviper#getPresenters(Class)}.
+ * To avoid that configure Moviper using {@link Moviper#setConfig(Config)} in {@link
+ * Application#onCreate()} with {@link Config.Builder#withPresenterAccessUtilEnabled(boolean)} set
+ * to true.
  */
 
 public class PresentersAccessUtilNotEnabled extends RuntimeException {
