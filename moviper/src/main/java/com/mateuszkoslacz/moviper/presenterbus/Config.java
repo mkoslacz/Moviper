18fa50ecbf75be0f73184f006e4fe45337c8e33a
mkoslacz <mateusz.koslacz@grupawp.pl>
2016-12-22 14:49:44 +0100

    [IPC][docs] add: documentation

--- moviper/src/main/java/com/mateuszkoslacz/moviper/presenterbus/Config.java
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/presenterbus/Config.java
@@ -1,7 +1,13 @@
 package com.mateuszkoslacz.moviper.presenterbus;
 
+import com.mateuszkoslacz.moviper.base.exception.PresenterInstancesAccessNotEnabled;
+import com.mateuszkoslacz.moviper.base.exception.PresentersAccessUtilNotEnabled;
+
 /**
  * Created by mateuszkoslacz on 29.10.2016.
+ *
+ * Pass it to {@link Moviper#setConfig(Config)} to enable IPC and IPC Presenter Instance Access in
+ * app.
  */
 public class Config {
 
@@ -9,20 +15,26 @@ public class Config {
 
     private boolean instancePresentersEnabled;
 
-    Config(){}
+    Config() {
+    }
 
     private Config(Builder builder) {
         presenterAccessUtilEnabled = builder.presenterAccessUtilEnabled;
         instancePresentersEnabled = builder.instancePresentersEnabled;
     }
 
-    public boolean isPresenterAccessUtilEnabled() {
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    boolean isPresenterAccessUtilEnabled() {
         return presenterAccessUtilEnabled;
     }
 
-    public boolean isInstancePresentersEnabled() {
+    boolean isInstancePresentersEnabled() {
         return instancePresentersEnabled;
     }
+
 
     public static final class Builder {
         private boolean presenterAccessUtilEnabled;
@@ -31,11 +43,20 @@ public class Config {
         public Builder() {
         }
 
+        /**
+         * Set it to true if you want to use IPC {@link Moviper#getPresenters(Class)}. Enabling it
+         * lets you avoid {@link PresentersAccessUtilNotEnabled} exceptions.
+         */
         public Builder withPresenterAccessUtilEnabled(boolean presenterAccessUtilEnabled) {
             this.presenterAccessUtilEnabled = presenterAccessUtilEnabled;
             return this;
         }
 
+        /**
+         * Set it to true if you want to use IPC Presenter Instance Access {@link
+         * Moviper#getPresenterInstance(Class, String)}. Enabling it lets you avoid {@link
+         * PresenterInstancesAccessNotEnabled} exceptions.
+         */
         public Builder withInstancePresentersEnabled(boolean instancePresentersEnabled) {
             this.instancePresentersEnabled = instancePresentersEnabled;
             return this;
