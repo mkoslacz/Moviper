e12973e7ce6e7cf57dfd7bd9f6205363bbccabd5
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-01-02 09:48:08 +0100

    [core][butterknife][passive] add: ButterKnife passive Fragment base classes

---
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/fragment/autoinject/passive/butterknife/ViperButterKnifePassiveFragment.java
@@ -0,0 +1,25 @@
+package com.mateuszkoslacz.moviper.base.view.fragment.autoinject.passive.butterknife;
+
+import android.view.View;
+
+import com.hannesdorfmann.mosby.mvp.MvpView;
+import com.mateuszkoslacz.moviper.base.view.fragment.autoinject.passive.ViperAiPassiveFragment;
+import com.mateuszkoslacz.moviper.iface.view.ViperView;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by tomasznajda on 14.12.2016.
+ */
+
+public abstract class ViperButterKnifePassiveFragment
+        <ViewType extends MvpView>
+        extends ViperAiPassiveFragment<ViewType>
+        implements ViperView {
+
+    @Override
+    protected void injectViews(View view) {
+        super.injectViews(view);
+        ButterKnife.bind(this, view);
+    }
+}
