cc5fd9fa8624ac81adb44add158b1b86a92564e0
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-01-02 09:47:31 +0100

    [core][data-binding] add: DataBinding Fragment base classes

---
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/fragment/autoinject/databinding/ViperDataBindingFragment.java
@@ -0,0 +1,35 @@
+package com.mateuszkoslacz.moviper.base.view.fragment.autoinject.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby.mvp.MvpView;
+import com.mateuszkoslacz.moviper.base.view.fragment.autoinject.ViperAiFragment;
+import com.mateuszkoslacz.moviper.iface.view.ViperView;
+
+/**
+ * Created by tomasznajda on 14.12.2016.
+ */
+
+public abstract class ViperDataBindingFragment
+        <ViewType extends MvpView,
+                Presenter extends MvpPresenter<ViewType>,
+                Binding extends ViewDataBinding>
+        extends ViperAiFragment<ViewType, Presenter>
+        implements ViperView {
+
+    protected Binding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false);
+        return mBinding.getRoot();
+    }
+}
---
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/fragment/autoinject/databinding/ViperDataBindingLceViewStateFragment.java
@@ -0,0 +1,39 @@
+package com.mateuszkoslacz.moviper.base.view.fragment.autoinject.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby.mvp.lce.MvpLceView;
+import com.hannesdorfmann.mosby.mvp.viewstate.ViewState;
+import com.mateuszkoslacz.moviper.base.view.fragment.autoinject.ViperLceViewStateAiFragment;
+import com.mateuszkoslacz.moviper.iface.view.ViperLceView;
+
+/**
+ * Created by tomasznajda on 14.12.2016.
+ */
+
+public abstract class ViperDataBindingLceViewStateFragment
+        <ContentView extends View,
+                Model,
+                ViewType extends MvpLceView<Model>,
+                Presenter extends MvpPresenter<ViewType>,
+                ViewStateType extends ViewState<ViewType>,
+                Binding extends ViewDataBinding>
+        extends ViperLceViewStateAiFragment<ContentView, Model, ViewType, Presenter, ViewStateType>
+        implements ViperLceView<Model> {
+
+    protected Binding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false);
+        return mBinding.getRoot();
+    }
+}
