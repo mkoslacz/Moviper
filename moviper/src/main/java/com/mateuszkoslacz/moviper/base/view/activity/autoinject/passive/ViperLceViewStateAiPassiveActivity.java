10088d80b76ab82eb7d98859c57113256416d23b
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-01-02 12:36:41 +0100

    [core][passive] fix: passive views initialization

--- moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/activity/autoinject/passive/ViperLceViewStateAiPassiveActivity.java
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/activity/autoinject/passive/ViperLceViewStateAiPassiveActivity.java
@@ -24,18 +24,16 @@ public abstract class ViperLceViewStateAiPassiveActivity
 
     /**
      * <b>DO NOT</b> use this method because of a fact that this view should be completely passive
-     * (independent from the presenter type)! <br/>
-     * Every call to this method will result in throwing
-     * {@link PresenterAccessFromPassiveView} exception. Instead you should use getters to provide
-     * event sources that will notify presenter after presenter's registration to them. To use
+     * (independent from the presenter type)! <br/> Instead you should use getters to provide event
+     * sources that will notify presenter after presenter's registration to them. To use
      * getPresenter() method you shall use non-passive Moviper view.
      *
-     * @return Nothing. It always throws {@link PresenterAccessFromPassiveView} exception.
+     * @return raw {@link ViperPresenter}, so you can't call any custom methods on it anyway
      */
     @NonNull
     @Override
     @Deprecated
     public ViperPresenter<ViewType> getPresenter() {
-        throw new PresenterAccessFromPassiveView(this);
+        return super.getPresenter();
     }
 }
--- moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/fragment/autoinject/passive/ViperAiPassiveFragment.java
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/base/view/fragment/autoinject/passive/ViperAiPassiveFragment.java
@@ -19,18 +19,17 @@ public abstract class ViperAiPassiveFragment<ViewType extends MvpView>
     /**
      * <b>DO NOT</b> use this method because of a fact that this view should be completely passive
      * (independent from the presenter type)! <br/>
-     * Every call to this method will result in throwing
-     * {@link PresenterAccessFromPassiveView} exception. Instead you should use getters to provide
+     * Instead you should use getters to provide
      * event sources that will notify presenter after presenter's registration to them. To use
      * getPresenter() method you shall use non-passive Moviper view.
      *
-     * @return Nothing. It always throws {@link PresenterAccessFromPassiveView} exception.
+     * @return raw {@link ViperPresenter}, so you can't call any custom methods on it anyway
      */
     @NonNull
     @Override
     @Deprecated
     public ViperPresenter<ViewType> getPresenter() {
-        throw new PresenterAccessFromPassiveView(this);
+        return super.getPresenter();
     }
 
 }
