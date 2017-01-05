2b6a86fef8e788833ad2c1ed82e2c245e0dcd894
Tomasz Najda <tomasz.najda@grupawp.pl>
2017-01-05 16:22:39 +0100

    [tests][sample-super-rx-databinding] improv: start activity from routing tests

--- sample-super-rx-databinding/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRoutingRobolectricTest.java
+++ sample-super-rx-databinding/src/test/java/com/mateuszkoslacz/moviper/rxsample/viper/routing/ListingRoutingRobolectricTest.java
@@ -4,17 +4,20 @@ import android.content.Intent;
 
 import com.mateuszkoslacz.moviper.rxsample.BuildConfig;
 import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
+import com.mateuszkoslacz.moviper.rxsample.viper.presenter.UserDetailsPresenter;
 import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.ListingActivity;
 import com.mateuszkoslacz.moviper.rxsample.viper.view.activity.UserDetailsActivity;
+import com.mateuszkoslacz.moviper.server.ActivityStarter;
+import com.mateuszkoslacz.moviper.server.MoviperPresentersDispatcher;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 
-import static com.mateuszkoslacz.moviper.rxsample.viper.view.activity.UserDetailsActivity.USER_EXTRA;
 import static org.junit.Assert.assertTrue;
 import static org.robolectric.Robolectric.setupActivity;
 
@@ -36,12 +39,35 @@ public class ListingRoutingRobolectricTest {
     }
 
     @Test
-    public void startUserDetailsActivity() throws Exception {
+    public void startUserDetailsActivityIntentTest() throws Exception {
+        MoviperPresentersDispatcher.setInstance(null);
         User user = new User();
         user.setLogin("testUser");
         mRouting.startUserDetailsActivity(user);
         Intent starter = new Intent(mListingActivity, UserDetailsActivity.class);
-        starter.putExtra(USER_EXTRA, user.getLogin());
+        starter.putExtra(UserDetailsActivity.USER_EXTRA, user.getLogin());
         assertTrue(Shadows.shadowOf(mListingActivity).getNextStartedActivity().filterEquals(starter));
+    }
+
+    @Test
+    public void startUserDetailsActivityUnitTest() throws Exception {
+        MoviperPresentersDispatcher mPresentersDispatcher = Mockito.mock(MoviperPresentersDispatcher.class);
+        MoviperPresentersDispatcher.setInstance(mPresentersDispatcher);
+
+        User user = new User();
+        user.setLogin("testUser");
+
+        mRouting.startUserDetailsActivity(user);
+
+        Intent starter = new Intent(mListingActivity, UserDetailsActivity.class);
+        starter.putExtra(UserDetailsActivity.USER_EXTRA, user.getLogin());
+
+        ActivityStarter activityStarter = ActivityStarter.newBuilder()
+                .withIntent(starter)
+                .withContext(mListingActivity)
+                .withPresenter(new UserDetailsPresenter(starter.getExtras()))
+                .build();
+
+        Mockito.verify(mPresentersDispatcher).startActivity(activityStarter);
     }
 }
