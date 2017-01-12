d26b79ab11ac793367d195eec5683311d8d5b1bc
Jakub Jodelka <jakub@jodelka.me>
2017-01-12 11:51:54 +0100

    [sample][service] add: sample for viper service

---
+++ sample-service/src/main/java/com/mateuszkoslacz/moviper/rxsample/data/retrofit/GitHubApiInterface.java
@@ -0,0 +1,20 @@
+package com.mateuszkoslacz.moviper.rxsample.data.retrofit;
+
+import com.mateuszkoslacz.moviper.rxsample.viper.entity.User;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+
+public interface GitHubApiInterface {
+
+    String GITHUB_API_URL = "https://api.github.com";
+
+    @GET("/users")
+    Observable<List<User>> getUsers();
+
+    @GET("/users/{username}")
+    Observable<User> getUserForUsername(@Path("username") String username);
+}
