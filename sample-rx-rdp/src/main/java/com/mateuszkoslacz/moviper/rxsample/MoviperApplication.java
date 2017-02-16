032d9fcee8c1d96281e8c38d0b70738a7c287f1c
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-02-16 14:57:31 +0100

    [sample-rx-rdp] fix: temporary add multidex

--- sample-rx-rdp/src/main/java/com/mateuszkoslacz/moviper/rxsample/MoviperApplication.java
+++ sample-rx-rdp/src/main/java/com/mateuszkoslacz/moviper/rxsample/MoviperApplication.java
@@ -1,10 +1,11 @@
 package com.mateuszkoslacz.moviper.rxsample;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.mateuszkoslacz.moviper.rxsample.di.DIProvider;
 
-public class MoviperApplication extends Application {
+public class MoviperApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
