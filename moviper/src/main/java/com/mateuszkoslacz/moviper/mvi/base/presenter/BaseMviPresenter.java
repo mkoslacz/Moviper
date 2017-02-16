2184a79d80d613c49703632b061587835532668f
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-02-16 09:54:02 +0100

    [core][presenter] add: base MVI (non-rx) presenter

---
+++ moviper/src/main/java/com/mateuszkoslacz/moviper/mvi/base/presenter/BaseMviPresenter.java
@@ -0,0 +1,85 @@
+package com.mateuszkoslacz.moviper.mvi.base.presenter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
+import com.hannesdorfmann.mosby3.mvp.MvpView;
+import com.mateuszkoslacz.moviper.iface.interactor.ViperInteractor;
+import com.mateuszkoslacz.moviper.iface.presenter.ViperPresenter;
+import com.mateuszkoslacz.moviper.iface.presenter.interactor.ViperPresenterForInteractor;
+import com.mateuszkoslacz.moviper.iface.presenter.routing.ViperPresenterForRouting;
+import com.mateuszkoslacz.moviper.iface.routing.ViperRouting;
+import com.mateuszkoslacz.moviper.iface.view.ViperView;
+
+/**
+ * Created by mateuszkoslacz on 08.08.2016.
+ * <p>
+ * This is a base presenter class for callback VIPER concept. (see {@link MvpBasePresenter})
+ * <p>
+ * It contains the business logic of given VIPER screen. and references Interactor and Routing to
+ * delegate them data and framework specific tasks.
+ * <p>
+ * You can use any Viper View with this class ({@link com.mateuszkoslacz.moviper.base.view.activity.ViperActivity},
+ * <br> {@link com.mateuszkoslacz.moviper.base.view.activity.ViperLceActivity}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.activity.ViperViewStateActivity}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.activity.ViperLceViewStateActivity}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.fragment.ViperFragment}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.fragment.ViperLceFragment}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.fragment.ViperLceViewStateFragment}, <br> {@link
+ * com.mateuszkoslacz.moviper.base.view.fragment.ViperViewStateFragment},)
+ */
+public abstract class BaseMviPresenter
+        <ViewType extends MvpView,  // I prefer readability rather than conventions
+                ViewStateType,
+                InteractorType extends ViperInteractor,
+                RoutingType extends ViperRouting>
+        extends CommonMviBasePresenter<ViewType, ViewStateType>
+        implements ViperPresenter<ViewType>,
+        ViperPresenterForInteractor<InteractorType>,
+        ViperPresenterForRouting<RoutingType> {
+
+    @NonNull
+    private RoutingType routing;
+
+    @NonNull
+    private InteractorType interactor;
+
+    public BaseMviPresenter() {
+        this(null);
+    }
+
+    public BaseMviPresenter(Bundle args) {
+        super(args);
+        this.routing = createRouting();
+        this.interactor = createInteractor();
+    }
+
+    @Override
+    public void attachView(ViewType view) {
+        super.attachView(view);
+        //noinspection unchecked
+        routing.attach((ViperView) view, this);
+        //noinspection unchecked
+        interactor.attach(this);
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+        super.detachView(retainInstance);
+        routing.detach(retainInstance);
+        interactor.detach(retainInstance);
+    }
+
+    @NonNull
+    @Override
+    public RoutingType getRouting() {
+        return routing;
+    }
+
+    @NonNull
+    @Override
+    public InteractorType getInteractor() {
+        return interactor;
+    }
+}
