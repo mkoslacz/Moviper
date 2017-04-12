5ebfe7de56e1ea26d1af2b3f818653bd4833fc38
mkoslacz <mateusz.koslacz@grupawp.pl>
2017-04-12 11:47:52 +0200

    [ViperAiViewHolder] improv: add bindViews method
    it allows to easily bind data from dataobject to views on ViewHolder bind

--- moviper-recyclerview/src/main/java/com/mateuszkoslacz/moviper/base/view/viewholder/autoinject/ViperAiViewHolder.java
+++ moviper-recyclerview/src/main/java/com/mateuszkoslacz/moviper/base/view/viewholder/autoinject/ViperAiViewHolder.java
@@ -1,5 +1,8 @@
 package com.mateuszkoslacz.moviper.base.view.viewholder.autoinject;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
 import com.mateuszkoslacz.moviper.base.view.viewholder.ViperViewHolder;
 import com.mateuszkoslacz.moviper.iface.presenter.ViperPresenter;
 import com.mateuszkoslacz.moviper.iface.view.MvpDataView;
@@ -19,5 +22,25 @@ public abstract class ViperAiViewHolder
         injectViews(itemView);
     }
 
-    abstract protected void injectViews(android.view.View itemView);
+    @Override
+    public void setDataObject(DataObject dataObject) {
+        super.setDataObject(dataObject);
+        bindViews();
+    }
+
+    /**
+     * It's called only once, on ViewHolder creation ({@link RecyclerView.Adapter#onCreateViewHolder(ViewGroup,
+     * int)}. Inject view references and other reusable components here.
+     */
+    protected void injectViews(android.view.View itemView) {
+        // stub
+    }
+
+    /**
+     * It's called multiple times, on ViewHolder binding ({@link RecyclerView.Adapter#onBindViewHolder(RecyclerView.ViewHolder,
+     * int)}}. Bind data from DataObject to views here.
+     */
+    protected void bindViews() {
+        // stub
+    }
 }
